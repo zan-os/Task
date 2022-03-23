f6b3f4a0157b013d5c17e03654dde6c9b79c079c
Fauzan Osama <osamafauzan33@gmail.com>
2022-03-23 15:40:36 +0700

    initial commit

---
+++ app/src/main/java/com/example/task/ui/FourthFragment.kt
@@ -0,0 +1,27 @@
+package com.example.task.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.task.databinding.FragmentFourthBinding
+
+class FourthFragment : Fragment() {
+    private var _binding: FragmentFourthBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentFourthBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
