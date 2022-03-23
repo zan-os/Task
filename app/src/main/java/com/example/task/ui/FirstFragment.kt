a4b14621d63ac3b842b78f5506c4963800e9ac82
Fauzan Osama <osamafauzan33@gmail.com>
2022-03-23 15:53:12 +0700

    adding navigation to second screen

--- app/src/main/java/com/example/task/ui/FirstFragment.kt
+++ app/src/main/java/com/example/task/ui/FirstFragment.kt
@@ -1,10 +1,12 @@
 package com.example.task.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.example.task.R
 import com.example.task.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
@@ -18,6 +20,23 @@ class FirstFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonFirstScreen.setOnClickListener {
+            val secondFragment = SecondFragment()
+            val fragmentManager = parentFragmentManager
+            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+
+            fragmentTransaction.replace(
+                R.id.frame_layout_container,
+                secondFragment,
+                SecondFragment::class.java.simpleName
+            )
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
     }
 
     override fun onDestroyView() {
