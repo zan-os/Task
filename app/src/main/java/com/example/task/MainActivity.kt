758f4eae5f445be1d4525bd7b73b99b1dad15c73
Fauzan Osama <osamafauzan33@gmail.com>
2022-03-23 16:03:23 +0700

    add fragment to frame container

--- app/src/main/java/com/example/task/MainActivity.kt
+++ app/src/main/java/com/example/task/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.task
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.task.ui.FirstFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        val firstFragment = FirstFragment()
+
+        fragmentTransaction.add(R.id.frame_layout_container, firstFragment, FirstFragment::class.java.simpleName)
+        fragmentTransaction.commit()
     }
 }
