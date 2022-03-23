419603a09d3ffa2a7e792175fc33fbc10fec52bc
Fauzan Osama <osamafauzan33@gmail.com>
2022-03-23 16:20:24 +0700

    passing name to third fragment using bundle

--- app/src/main/java/com/example/task/ui/SecondFragment.kt
+++ app/src/main/java/com/example/task/ui/SecondFragment.kt
@@ -1,10 +1,11 @@
 package com.example.task.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.example.task.R
 import com.example.task.databinding.FragmentSecondBinding
 
@@ -19,6 +20,28 @@ class SecondFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonSecondScreen.setOnClickListener {
+            val thirdFragment = ThirdFragment()
+            val fragmentManager = parentFragmentManager
+            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+
+            val getNameInput = binding.editTextName.text.toString()
+            val bundle = Bundle()
+            bundle.putString(ThirdFragment.EXTRA_NAME, getNameInput)
+            thirdFragment.arguments = bundle
+
+            fragmentTransaction.replace(
+                R.id.frame_layout_container,
+                thirdFragment,
+                ThirdFragment::class.java.simpleName
+            )
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
     }
 
     override fun onDestroyView() {
