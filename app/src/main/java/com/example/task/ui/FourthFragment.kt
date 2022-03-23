59ff7abc38ce1ae92fbc38fa5477d656cdea094e
Fauzan Osama <osamafauzan33@gmail.com>
2022-03-23 18:36:51 +0700

    send data to third screen setFragmentResult

--- app/src/main/java/com/example/task/ui/FourthFragment.kt
+++ app/src/main/java/com/example/task/ui/FourthFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.setFragmentResult
 import com.example.task.databinding.FragmentFourthBinding
 
 class FourthFragment : Fragment() {
@@ -18,6 +20,32 @@ class FourthFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentFourthBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonFourthScreen.setOnClickListener {
+            val thirdFragment = ThirdFragment()
+            val fragmentManager = parentFragmentManager
+            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+
+            val getAgeInput = binding.editTextAge.text.toString().toInt()
+            val getAddressInput = binding.editTextAddress.text.toString()
+            val getJobInput = binding.editTextJob.text.toString()
+
+            val bundle = Bundle().apply {
+                putString(ThirdFragment.EXTRA_ADDRESS, getAddressInput)
+                putInt(ThirdFragment.EXTRA_AGE, getAgeInput)
+                putString(ThirdFragment.EXTRA_JOB, getJobInput)
+            }
+
+            thirdFragment.arguments = bundle
+
+            setFragmentResult(ThirdFragment.REQUEST_KEY, bundle)
+
+            fragmentManager.popBackStack()
+            fragmentTransaction.commit()
+        }
     }
 
     override fun onDestroyView() {
